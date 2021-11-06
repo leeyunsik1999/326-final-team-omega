71a43499466ab50a9c007afb531e3cb6997133c2
Yunsik Edwin Lee <leeyunsik1999@gmail.com>
2021-11-06 15:27:48 -0400

    pls

--- server/server.js
+++ server/server.js
@@ -227,8 +227,7 @@ app.get('/user/:id/date', (req, res) => {
 });
 
 app.get("*", (req, res) => {
-    console.log(__dirname+"../client/index.html")
-    res.sendFile(__dirname+"../client/index.html");
+    res.sendFile("../client/index.html");
 });
 
 app.listen(process.env.PORT || 443, () => {
