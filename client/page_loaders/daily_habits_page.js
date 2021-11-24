a46aac53745550f1123be3fa7f917fca8645571e
Erin <57811576+esong2001@users.noreply.github.com>
2021-11-24 16:28:33 -0500

    js

--- client/page_loaders/daily_habits_page.js
+++ client/page_loaders/daily_habits_page.js
@@ -1,75 +1,139 @@
-export function load_daily_page(parent) {
-
-  const habits_page = document.createElement("div");
-  habits_page.id = "habits-page";
-  habits_page.classList.add("page", "container");
-  parent.appendChild(habits_page);
-
-  const row = document.createElement("div");
-  habits_page.appendChild(row);
-
-  const habit_to_do = document.createElement("div");
-  habit_to_do.id = "habit_to_do";
-  habit_to_do.classList.add("col-4");
-  row.appendChild(habit_to_do);
-
-  const habit_card = document.createElement("div");
-  habit_card.id = "habit_card";
-  habit_card.classList.add("card");
-  habit_to_do.appendChild(habit_card);
-
-  const card_body = document.createElement("div");
-  card_body.classList.add("card-body");
-  habit_card.appendChild(card_body);
-
-  const date_text = document.createElement("h1");
-  // document.body.appendChild(document.innerText(`${get_date_text()}`));
-  card_body.appendChild(date_text);
-
-  const habits_list = document.createElement("div");
-  // const event_response = await fetch(`${window.hostname}/user/${window.user_name}/${get_date()}/events}/`)
-  // if(event_response.status !== 200) {
-  //   alert("An error has occured.");
-  // } else {
-  //   const events = await event_response.json()
-  //   for(const event in events) {
-  //     habits_list.appendChild(create_habit(event));
-  //   }
-  // }
-  card_body.appendChild(habits_list);
-
-  habit_card.appendChild(document.createElement("br"));
-
-  const add_habit_sub_button = document.createElement("div");
-  add_habit_sub_button.classList.add("add-habit-btn", "d-flex", "justify-content-center", "sub-button-primary");
-  card_body.appendChild(add_habit_sub_button);
-  const plus_image = document.createElement("img");
-  plus_image.id = "plus_image";
-  plus_image.src = "./images/add_logo.png";
-  add_habit_sub_button.appendChild(plus_image);
-
-  habit_card.appendChild(document.createElement("br"));
-
-  const monthly_page_button = document.createElement("div");
-  monthly_page_button.id = "monthly_page_button";
-  monthly_page_button.classList.add("d-flex", "justify-content-center");
-  habit_card.appendChild(monthly_page_button);
-  const button = document.createElement("button");
-  button.classList.add("btn", "btn-light");
-  button.innerText = "Monthly View";
-  monthly_page_button.appendChild(button);
-
-  const daily_photos = document.createElement("div");
-  daily_photos.id = "daily_photos";
-  daily_photos.classList.add("col-8");
-  habit_to_do.appendChild(daily_photos);
-
+const month_word = {
+  1 : "January",
+  2 : "February",
+  3 : "March",
+  4 : "April",
+  5 : "May",
+  6 : "June",
+  7 : "July",
+  8 : "August",
+  9 : "September",
+  10 : "October",
+  11 : "November",
+  12 : "December"
 }
 
-function create_habit(habit_title) {
+export function load_daily_page(parent) {
+
+  (async () => {
+    const habits_page = document.createElement("div");
+    habits_page.id = "habits-page";
+    habits_page.classList.add("page", "container");
+    parent.appendChild(habits_page);
+
+    const row = document.createElement("div");
+    row.classList.add("row")
+    habits_page.appendChild(row);
+
+    const habit_to_do = document.createElement("div");
+    habit_to_do.id = "habit-to-do";
+    habit_to_do.classList.add("col-4");
+    row.appendChild(habit_to_do);
+
+    const habit_card = document.createElement("div");
+    habit_card.id = "habit-card";
+    habit_card.classList.add("card");
+    habit_to_do.appendChild(habit_card);
+
+    const card_body = document.createElement("div");
+    card_body.classList.add("card-body");
+    habit_card.appendChild(card_body);
+
+    const date_text = document.createElement("h1");
+    document.body.appendChild(document.innerText = `${get_date_text}`);
+    card_body.appendChild(date_text);
+
+    const habits_list = document.createElement("div");
+    habits_list.id = "habits-list";
+    const event_response = await fetch(`${window.hostname}/user/${window.user_name}/${get_date}/events}/`)
+    if(event_response.status !== 200) {
+      alert("An error has occured.");
+    } else {
+      const events = await event_response.json();
+      for(const event in events) {
+        habits_list.appendChild(create_habit(event));
+      }
+    }
+    card_body.appendChild(habits_list);
+
+    habit_card.appendChild(document.createElement("br"));
+
+    const add_habit_sub_button = document.createElement("div");
+    add_habit_sub_button.id = "add-habit-sub-button";
+    add_habit_sub_button.classList.add("add-habit-btn", "d-flex", "justify-content-center", "sub-button-primary");
+    card_body.appendChild(add_habit_sub_button);
+    const plus_image = document.createElement("img");
+    plus_image.id = "add-button-logo";
+    plus_image.src = "./images/add_logo.png";
+    add_habit_sub_button.appendChild(plus_image);
+
+    habit_card.appendChild(document.createElement("br"));
+
+    const monthly_page_button = document.createElement("div");
+    monthly_page_button.id = "monthly-page-button";
+    monthly_page_button.classList.add("d-flex", "justify-content-center");
+    habit_card.appendChild(monthly_page_button);
+    const button = document.createElement("button");
+    button.classList.add("btn", "btn-light");
+    button.type = "button";
+    button.innerText = "Monthly View";
+    monthly_page_button.appendChild(button);
+
+    const daily_photos = document.createElement("div");
+    daily_photos.id = "daily-photos";
+    daily_photos.classList.add("col-8");
+    habit_to_do.appendChild(daily_photos);
+
+    const photo_card = document.createElement("div")
+    photo_card.classList.add("card");
+    daily_photos.appendChild(photo_card);
+
+    const photo_card_body = document.createElement("div");
+    photo_card_body.classList.add("card-body");
+    photo_card.appendChild(photo_card_body);
+
+    const photo_card_container = document.createElement("div");
+    photo_card_container.classList.add("container");
+    photo_card_body.appendChild(photo_card_container);
+
+    const image_row = document.createElement("div");
+    image_row.id = "image-row";
+    image_row.classList.add("row");
+    photo_card_container.appendChild(image_row);
+
+    // images
+
+    const add_pic_sub_button = document.createElement("div");
+    add_pic_sub_button.id = "add-pic-sub-button";
+    add_pic_sub_button.classList.add("add-pic-btn", "d-flex", "justify-content-center", "sub-button-primary");
+    photo_card_container.appendChild(add_pic_sub_button);
+
+    const add_button_logo = document.createElement("div");
+    add_button_logo.id = "add-button-logo";
+    add_button_logo.src = "./images/add_logo.png";
+  })();
+}
+
+function get_date() {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0');
+  let yyyy = today.getFullYear(); 
+  return yyyy + mm + dd;
+}
+
+function get_date_text() {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0');
+  let yyyy = today.getFullYear(); 
+  return month_word[mm] + ", " + dd + " " + yyyy;
+}
+
+function create_habit(habit_title, parent) {
   const task = document.createElement("div");
   task.classList.add("custom-control", "custom-checkbox");
-  habits_list.appendChild(task);
+  parent.appendChild(task);
 
   const checkbox = document.createElement("input");
   checkbox.id = habit_title;
@@ -84,3 +148,14 @@ function create_habit(habit_title) {
   task.appendChild(label);
 }
 
+function create_image(image_link, parent) {
+  const image_col = document.createElement("div");
+  image_col.classList.add("col");
+  parent.appendChild(image_col);
+
+  for (let i = 0; i < image_link.length; i++) {
+    const add_image = document.createElement("img");
+    add_image.src = image_link[i];
+    image_col.appendChild(add_image);
+  }
+}
---
+++ client/page_loaders/monthly_habits_page.js
@@ -0,0 +1,121 @@
+const months = {
+  1 : 31,
+  2 : 28,
+  3 : 31,
+  4 : 30,
+  5 : 31,
+  6 : 30,
+  7 : 31,
+  8 : 31,
+  9 : 30,
+  10 : 31,
+  11 : 30,
+  12 : 31
+}
+
+const month_word = {
+  1 : "JAN",
+  2 : "FEB",
+  3 : "MAR",
+  4 : "APR",
+  5 : "MAY",
+  6 : "JUN",
+  7 : "JUL",
+  8 : "AUG",
+  9 : "SEP",
+  10 : "OCT",
+  11 : "NOV",
+  12 : "DEC"
+}
+
+
+function get_month() {
+  let today = new Date();
+  return String(today.getMonth() + 1).padStart(2, '0');
+}
+
+export function load_monthly_page(parent) {
+  (async () => {
+    const month_days = months[get_month];
+    let habit_list;
+    const event_response = await fetch(`${window.hostname}/user/${window.user_name}/${month_word[get_month]}/events}/`);
+    if(event_response.status !== 200) {
+      alert("An error has occured.");
+    } else {
+      habit_list = await event_response.json();
+    }
+
+    const monthly_habits_page = document.createElement("div");
+    monthly_habits_page.id = "monthly-habits-page";
+    monthly_habits_page.classList.add("page");
+    parent.appendChild(monthly_habits_page);
+
+    const grid_container = document.createElement("div");
+    grid_container.id = "grid-container";
+    grid_container.classList.add("container");
+    monthly_habits_page.appendChild(grid_container);
+
+    const habits_month = document.createElement("h1");
+    habits_month.id = "habits-month";
+    habits_month.innerText = month_word[get_month];
+    grid_container.appendChild(habits_month);
+
+    add_br(grid_container);
+
+    for(let i = 0; i < habit_list.length; i++) {
+      add_habit(grid_container, habit_list[i]["name"], month_days);
+    }
+
+    add_br(grid_container);
+
+    const daily_page_button = document.createElement("div");
+    daily_page_button.id = "daily-page-button";
+    daily_page_button.classList.add("d-flex", "justify-content-center");
+    grid_container.appendChild(daily_page_button);
+
+    const daily_button = document.createElement("button");
+    daily_button.type = "button";
+    daily_button.classList.add("btn", "btn-light");
+    daily_button.innerText = "Daily View";
+    daily_page_button.appendChild(daily_button);
+  })();
+
+}
+
+function add_br(parent) {
+  const br = document.createElement("br");
+  parent.appendChild(br);
+}
+
+function add_habit(parent, habit_name, month_days) {
+  const row = document.createElement("div");
+  row.classList.add("row");
+  parent.appendChild(row);
+
+  const col_2 = document.createElement("div");
+  col_2.classList.add("col-2");
+  row.appendChild(col_2);
+
+  const habit_label = document.createElement("p");
+  habit_label.id = "habit-id";
+  col_2.appendChild(habit_label);
+
+  const habit_text = document.createElement("b");
+  habit_text.innerText = habit_name;
+  habit_label.appendChild(habit_text);
+
+  const col_10 = document.createElement("div");
+  col_10.classList.add("col-2");
+  row.appendChild(col_10);
+
+  const grid = document.createElement("div");
+  grid.classList.add("grid");
+  col_10.appendChild(grid);
+
+  for(let i = 0; i < month_days; i++) {
+    const day = document.createElement("div");
+    day.classList.add("day");
+    // day.innerText = i;
+    grid.appendChild(day);
+  }
+}
